1a79f91786519494da6cc7608a518ab7638bb9a2
Matthew Avant <matthew.avant@gmail.com>
2014-06-15 13:51:49 -0400

    Added hotkeys for luakit (ctrl+u) and surf mightytext.net/web (ctrl+y)

--- config.def.h
+++ config.def.h
@@ -53,11 +53,15 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *browser[]  = { "luakit", NULL };
+static const char *texts[]  = { "surf mightytext.net/web", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,	                XK_y,	   spawn,          {.v = texts } },
+	{ MODKEY,		        XK_u,      spawn,          {.v = browser } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
